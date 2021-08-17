d276117f4315aba13242d0050a93ac6ffffea45c
ttucker <tracytuckeremail@gmail.com>
2021-08-17 18:19:49 -0500

    Sign In Authentication setup

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import firebase from 'firebase'
+import Button from 'react-bootstrap/Button';
 
 function SignIn() {
+    function signInWithGoogle() {
+        const provider = new firebase.auth.GoogleAuthProvider()
+    }
     return (
         <div>
-            
+            <Button onclick={signInWithGoogle()}>Sign In With Google</Button>
         </div>
     )
 }
