2ea7386cd8da7be03859449c1bd756251ba7ca6e
ttucker <tracytuckeremail@gmail.com>
2021-08-18 03:58:34 -0500

    chat creation for photoURL and text

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { db } from '../firebase'
 import SignOut from './SignOut'
 
 function Chat() {
+    const [messages, setMessages] = useState([])
+    useEffect(() => {
+        db.collection('messages').orderBy('createdAt').limit(50).onSnapshot(snapshot => {
+            setMessages(snapshot.docs.map(doc => doc.data()))
+        })
+    }, [])
     return (
         <div>
             <SignOut />
+            {messages.map(({ id, text, photoURL, uid }) => (
+                <div>
+                    <img src={photoURL} alt="" />
+                    <p>{text}</p>
+                </div>
+            ))}
         </div>
     )
 }
